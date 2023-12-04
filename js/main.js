5648c1b3bf703fe9aa738d3bc35f8872433615ef
Janice Walker <walker.janice@aol.com>
2023-12-04 13:53:54 -0600

    modal

--- js/main.js
+++ js/main.js
@@ -1,63 +1,65 @@
 import { renderMovies, saveChanges} from '../api/render.js';
 
-
 //MAIN
+
+// Function to open the 'Add Movie' modal
+const openAddMovieModal = () => {
+    const modal = new bootstrap.Modal(document.getElementById('addMovieModal'));
+    modal.show();
+};
+
+// Function to save a new movie
+// Function to save a new movie locally
+const saveNewMovie = async () => {
+    try {
+        const addTitle = document.getElementById('addTitle').value;
+        const addRating = parseFloat(document.getElementById('addRating').value);
+
+        if (!addTitle || isNaN(addRating)) {
+            // Validation failed, show an error message or handle it accordingly
+            console.error('Please fill out all required fields.');
+            return;
+        }
+
+        const newMovie = {
+            title: addTitle,
+            rating: addRating,
+            // You can add other properties if needed
+        };
+
+        // Retrieve existing movies from localStorage or initialize an empty array
+        const existingMovies = JSON.parse(localStorage.getItem('movies')) || [];
+
+        // Add the new movie to the array
+        existingMovies.push(newMovie);
+
+        // Save the updated array back to localStorage
+        localStorage.setItem('movies', JSON.stringify(existingMovies));
+
+        // Close the modal after saving
+        const modal = new bootstrap.Modal(document.getElementById('addMovieModal'));
+        modal.hide();
+
+        // Refresh the movie list
+        await renderMovies();
+    } catch (error) {
+        console.error('Error saving new movie:', error);
+    }
+};
+
+
+// IIFE (Immediately Invoked Function Expression)
 (async () => {
-    await renderMovies()
-    const saveChangesButton = document.getElementById('saveChangesButton');
-    saveChangesButton.addEventListener('click', () => saveChanges());
+        // Render movies when the DOM is loaded
+        await renderMovies();
+
+        // Event listener for the 'Add Movie' button
+        document.getElementById('addMovieButton').addEventListener('click', () => openAddMovieModal());
+
+        // Event listener for the 'Save New Movie' button
+        document.getElementById('saveNewMovieButton').addEventListener('click', () => saveNewMovie());
+
+        // Attach an event listener to the 'Save Changes' button
+        const saveChangesButton = document.getElementById('saveChangesButton');
+        saveChangesButton.addEventListener('click', () => saveChanges());
 })();
-
-
-const getSearchResults = async (query) => {
-    try {
-        // Perform an API call or any other logic to get search results based on the query
-        // Replace the following line with your actual implementation
-        const response = await fetch('http://localhost:3000/movies');
-        const data = await response.json();
-
-        // Assuming the API response contains an array of search results
-        return data.results;
-    } catch (error) {
-        console.error('Error fetching search results:', error);
-        throw error; // Re-throw the error to be caught in the calling function
-    }
-};
-// Function to search movies based on the query
-const searchMovies = async (query) => {
-    // Implement the logic to search movies based on the query
-    // For example, you can use an API or search within the existing movies array
-    // Update the movieList with the search results
-    // You can use the renderMovies function to display the search results
-    const movieList = document.getElementById('movies-list');
-    movieList.innerHTML = ''; // Clear the existing movie list
-
-    try {
-        // Implement the logic to get search results based on the query
-        const searchResults = await getSearchResults(query);
-
-        searchResults.forEach(movie => {
-            // Render each movie card similar to how it's done in renderMovies
-            // ...
-
-            // Append the card to the movie list
-            movieList.appendChild(card);
-        });
-    } catch (error) {
-        console.error('Error searching movies:', error);
-        movieList.innerHTML = '<p>Error loading search results</p>';
-    }
-};
-
-
-const searchForm = document.getElementById('searchForm');
-searchForm.addEventListener('submit', async (event) => {
-    event.preventDefault(); // Prevent the default form submission
-
-    // Get the search input value
-    const searchInput = document.getElementById('searchInput');
-    const searchQuery = searchInput.value.trim();
-
-    // Call the searchMovies function with the entered query
-    await searchMovies(searchQuery);
-});
